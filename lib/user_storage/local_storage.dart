16f3847d158d9ecdb1444a65963d3dc0ee5315a7
Matthew Rattigan <mattrattigan@protonmail.com>
2023-11-04 18:56:10 -0400

    redue changes

--- lib/user_storage/local_storage.dart
+++ lib/user_storage/local_storage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:no_name_app/user_model/users.dart';
 
@@ -16,7 +17,33 @@ class LocalStorage {
     return _storage.delete(key: key);
   }
 
-  Future? deleteAllFromStorage({required String key}) {
+  Future<void> deleteAllFromStorage() {
     return _storage.deleteAll();
   }
+
+Future<void> saveUser(ChoiceUser choiceUser) async {
+    final userJson = choiceUser.toJson(); // This should return a JSON string.
+    await _storage.write(key: 'user', value: userJson);
+}
+
+
+  Future<ChoiceUser?> readUser() async {
+    try {
+      final userJson = await _storage.read(key: 'user');
+      print('Raw User JSON: $userJson');
+      if (userJson != null) {
+        return ChoiceUser.fromJson(userJson);
+      }
+    } on ChoiceUserCreation catch (e) {
+      print("\n${e.toString()}\n");
+    } catch (e) {
+      print("Error reading user: $e");
+      rethrow;
+    }
+    return null;
+  }
+
+  // Future<UserCredential> convertToUser() async {
+    
+  // }
 }
